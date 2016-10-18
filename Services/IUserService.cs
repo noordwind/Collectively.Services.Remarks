293b018576b69ff4031d403482f2c44f5007dc67
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-18 13:55:24 +0200

    Init

---
+++ Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Coolector.Services.Remarks.Services
+{
+    public interface IUserService
+    {
+        Task CreateIfNotFoundAsync(string userId, string name);
+    }
+}
