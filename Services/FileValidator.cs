bc3a1a3eb80876990064fb8a9213702b1ecae2e1
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-19 22:08:38 +0200

    Temporary removed drawing reference

--- Services/FileValidator.cs
+++ Services/FileValidator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.IO;
 using File = Coolector.Services.Remarks.Domain.File;
 
@@ -7,15 +6,18 @@ namespace Coolector.Services.Remarks.Services
 {
     public class FileValidator : IFileValidator
     {
+        //TODO: Fix this
         public bool IsImage(File file)
         {
             try
             {
                 using (var stream = new MemoryStream(file.Bytes))
                 {
-                    var image = Image.FromStream(stream);
+                    //var image = Image.FromStream(stream);
 
-                    return image.Width > 0 && image.Height > 0;
+                    //return image.Width > 0 && image.Height > 0;
+
+                    return true;
                 }
             }
             catch (Exception exception)
