f9ac20b49e75ee3fac04d402cc2ed80a80059e5a
spetz <spetz90@gmail.com>
2016-12-06 10:07:47 +0100

    Fixed operation code name

--- Coolector.Services.Remarks/OperationCodes.cs
+++ Coolector.Services.Remarks/OperationCodes.cs
@@ -6,7 +6,7 @@
         public static string RemarkNotFound => "remark_not_found";
         public static string UserNotAllowedToDeleteRemark => "user_not_allowed_to_delete_remark";
         public static string DistanceBetweenUserAndRemarkIsTooBig => "distance_between_user_and_remark_is_too_big";
-        public static string InvalidFile => "invalid_fille";
+        public static string InvalidFile => "invalid_file";
         public static string CannotConvertFile => "cannot_convert_file";
         public static string Error => "error";
     }
