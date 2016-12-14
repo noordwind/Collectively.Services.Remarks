7578402f81e94d418159df97e21f40897c145b7b
spetz <spetz90@gmail.com>
2016-12-14 13:02:47 +0100

    Publishing new remark info to Facebook wall

--- Coolector.Services.Remarks/Handlers/CreateRemarkHandler.cs
+++ Coolector.Services.Remarks/Handlers/CreateRemarkHandler.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Coolector.Common;
 using Coolector.Common.Commands;
 using Coolector.Common.Domain;
 using Coolector.Common.Services;
@@ -14,7 +12,6 @@ using Coolector.Services.Remarks.Shared.Events;
 using Coolector.Services.Users.Shared.Commands;
 using Lockbox.Client.Extensions;
 using NLog;
-using Polly;
 using RawRabbit;
 using RemarkFile = Coolector.Services.Remarks.Shared.Events.Models.RemarkFile;
 
@@ -73,13 +70,13 @@ namespace Coolector.Services.Remarks.Handlers
                 .OnSuccess(async () =>
                 {
                     var remark = await _remarkService.GetAsync(command.RemarkId);
+                    await PublishOnSocialMediaAsync(remark.Value, command.SocialMedia);
                     await _bus.PublishAsync(new RemarkCreated(command.Request.Id, command.RemarkId,
                         command.UserId, remark.Value.Author.Name,
                         new RemarkCreated.RemarkCategory(remark.Value.Category.Id, remark.Value.Category.Name),
                         new RemarkCreated.RemarkLocation(remark.Value.Location.Address, command.Latitude, command.Longitude),
                         remark.Value.Photos.Select(x => new RemarkFile(x.Name, x.Size, x.Url, x.Metadata)).ToArray(),
                         command.Description, remark.Value.CreatedAt));
-                    await PublishOnSocialMedia(remark.Value, command.SocialMedia);
                 })
                 .OnCustomError(ex => _bus.PublishAsync(new CreateRemarkRejected(command.Request.Id,
                     command.RemarkId, command.UserId, ex.Code, ex.Message)))
@@ -92,9 +89,9 @@ namespace Coolector.Services.Remarks.Handlers
                 .ExecuteAsync();
         }
 
-        private async Task PublishOnSocialMedia(Remark remark, List<SocialMedia> socialMedia)
+        private async Task PublishOnSocialMediaAsync(Remark remark, List<SocialMedia> socialMedia)
         {
-            if (socialMedia == null)
+            if (socialMedia == null || !socialMedia.Any())
                 return;
 
             foreach (var service in socialMedia.Where(x => x.Name.NotEmpty() && x.Publish))
@@ -107,7 +104,7 @@ namespace Coolector.Services.Remarks.Handlers
                             Request = Request.New<PostOnFacebookWall>(),
                             UserId = remark.Author.UserId,
                             AccessToken = service.AccessToken,
-                            Message = $"Test remark: {remark.Category}."
+                            Message = $"I've just sent a new remark for category {remark.Category} using Coolector."
                         });
                         break;
                 }
