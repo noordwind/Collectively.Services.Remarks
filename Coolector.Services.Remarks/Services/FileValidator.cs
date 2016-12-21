a41470639c94cba562d471b9355e724debb04403
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-12-21 10:13:41 +0100

    file validator fix

--- Coolector.Services.Remarks/Services/FileValidator.cs
+++ Coolector.Services.Remarks/Services/FileValidator.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using System.IO;
+using NLog;
+using Structure.Sketching;
 using File = Coolector.Services.Remarks.Domain.File;
 
 namespace Coolector.Services.Remarks.Services
 {
     public class FileValidator : IFileValidator
     {
-        //TODO: Fix this
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public bool IsImage(File file)
         {
             try
             {
                 using (var stream = new MemoryStream(file.Bytes))
                 {
-                    //var image = Image.FromStream(stream);
+                    var image = new Image(stream);
 
-                    //return image.Width > 0 && image.Height > 0;
-
-                    return true;
+                    return image.Width > 0 && image.Height > 0;
                 }
             }
             catch (Exception exception)
             {
+                Logger.Error(exception, "Error while reading image from stream");
                 return false;
             }
         }
