31d0c490370b28ee4d98fb185cc3a36d69782a66
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-01-02 13:08:51 +0100

    remark states

---
+++ Coolector.Services.Remarks.Shared/RemarkStates.cs
@@ -0,0 +1,9 @@
+﻿namespace Coolector.Services.Remarks.Shared
+{
+    public class RemarkStates
+    {
+        public static readonly string All = "all";
+        public static readonly string Active = "active";
+        public static readonly string Resolved = "resolved";
+    }
+}
